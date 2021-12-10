407d3438ac463f5196df43fb4fdab4e04ef661c8
Kutay Sütçü <94748829+SutcuKutay@users.noreply.github.com>
2021-12-10 20:49:17 +0300

    Projenin son hali
    Main içinde olabildiğince az kod bırakıldı.

--- main.c
+++ main.c
@@ -1,38 +1,59 @@
-#include <stdio.h>//for printf, etc
-#include <unistd.h>//for fork
+#include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
-#include <string.h>//for strlen
-#include <sys/types.h>//waitpid fonksiyonu
-#include <sys/wait.h>//waitpid fonksiyonu
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+char komut[80];
+char* komutlar[10];
+char yol[100000];
+int pidler[5];
+int sayac = 0;
+
+void prompt(){//prompt yazar
+	printf("\x1b[31m" "%s: " "\x1b[0m" "\x1b[32m" "sau > " "\x1b[0m", yol);
+};
+
+void kullanici_girisi(){//kullanicinin girdigi string ifadelerle ilgilenir
+	fgets(komut, sizeof(komut), stdin);//girilen string ifadeyi cekme
+	komut[strlen(komut) - 1] = '\0';//string ifadenin sonundaki bosluk karakterini silme
+	komutlar[0]=strtok(komut," ");
+	for(int i = 1; i < 10; i++){
+		komutlar[i]=strtok(NULL," ");
+	}
+};
+
+void sonlandir(){//kabugu sonlandirir
+	printf("exit\n");
+	exit(0);
+};
+
+void gezinme(){//klasorler arasinda gezinme saglar
+	chdir(komutlar[1]);
+	getcwd(yol, sizeof(yol));
+};
+
+void pidleri_yazdir(){//en son olusan 5 pid'i ekrana yazdirir
+	for(int i=0;i<5;i++){
+		printf("%d\n",pidler[i]);
+	}
+};
 
 int main()
 {
-	char komut[80];
-	char* komutlar[10];
-	char yol[100000];
-	int pidler[5];
-	int sayac = 0;
 	getcwd(yol, sizeof(yol));//dosya yolunu dondurur
 	while(1){
-		printf("\x1b[31m" "%s: " "\x1b[0m" "\x1b[32m" "sau > " "\x1b[0m", yol);//prompt kısmı
-		fgets(komut, sizeof(komut), stdin);//girilen string ifadeyi cekme
-		komut[strlen(komut) - 1] = '\0';//string ifadenin sonundaki bosluk karakterini silme
-		komutlar[0]=strtok(komut," ");
-		for(int i = 1; i < 10; i++){
-			komutlar[i]=strtok(NULL," ");
+		prompt();
+		kullanici_girisi();
+		if(strcmp(komutlar[0],"exit") == 0){
+			sonlandir();
 		}
-		if(strcmp(komutlar[0],"exit") == 0){ //terminali kapatma
-			printf("exit\n");
-			exit(0);
+		else if(strcmp(komutlar[0],"cd") == 0){
+			gezinme();
 		}
-		else if(strcmp(komutlar[0],"cd") == 0){ //dosyalar arasinda gezinme
-			chdir(komutlar[1]);
-			getcwd(yol, sizeof(yol));
-		}
-		else if(strcmp(komutlar[0],"showpid")==0){ //son 5 pid'i ekrana yazdirma
-			for(int i=0;i<5;i++){
-				printf("%d\n",pidler[i]);
-			}
+		else if(strcmp(komutlar[0],"showpid")==0){
+			pidleri_yazdir();
 		}
 		else{
 			int pid = fork();
